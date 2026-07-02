ed5d157f3a3f21f406dab728c36ec4b83d74dda4
JOHN JAIRO SILVA TRIVINO <jsilvat2@msuteq.edu.ec>
2026-07-02 16:26:03 -0500

    End commit

--- app/src/main/java/com/uteq/software/googlemaps/MainActivity.java
+++ app/src/main/java/com/uteq/software/googlemaps/MainActivity.java
@@ -14,14 +14,14 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
-
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     GoogleMap mapa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,61 +38,40 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                         .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-         mapa = googleMap;
+        mapa = googleMap;
 
         mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         mapa.getUiSettings().setZoomControlsEnabled(true);
+
         CameraUpdate camUpd1 =
-
                 CameraUpdateFactory
-
                         .newLatLngZoom(new LatLng(-1.0125791262991217, -79.46931397331672), 17);
-
         mapa.moveCamera(camUpd1);
-        LatLng madrid = new LatLng(27.17372149972348, 78.04217708611833);
 
-        CameraPosition camPos = new CameraPosition.Builder()
-
-                .target(madrid)
-
-                .zoom(19)
-
-                .bearing(45) //noreste arriba
-
-                .tilt(70) //punto de vista de la cámara 70 grados
-
-                .build();
-
-        CameraUpdate camUpd3 =
-
-                CameraUpdateFactory.newCameraPosition(camPos);
-
-        mapa.animateCamera(camUpd3);
-
-        PolylineOptions lineas = new
-                PolylineOptions()
-
+        PolylineOptions lineas = new PolylineOptions()
                 .add(new LatLng(-1.0120645693134682, -79.47161786876636))
-
                 .add(new LatLng(-1.0123327483556732, -79.46734779188552))
-
-                .add(new LatLng(-1.0132123754584286, -79.47168224178466))
-
                 .add(new LatLng(-1.013502008720489, -79.46734779188552))
-
+                .add(new LatLng(-1.0132123754584286, -79.47168224178466))
                 .add(new LatLng(-1.0120645693134682, -79.47161786876636));
-
         lineas.width(8);
-
         lineas.color(Color.RED);
-
         mapa.addPolyline(lineas);
 
         LatLng punto = new LatLng(-1.0128261977353945, -79.46916096523441);
-        mapa.addMarker(new
-                MarkerOptions().position(punto)
+        mapa.addMarker(new MarkerOptions()
+                .position(punto)
                 .title("UTEQ"));
+
+        mapa.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                marker.showInfoWindow();
+                return true;
+            }
+        });
     }
 }
